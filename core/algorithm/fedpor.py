834bb5d3ecf9e0d624846239fced9e806b965522
lokinko <lokinko.cs@gmail.com>
2025-01-20 02:46:02 +0000

    algorithm(fedpor): add the implements of fedpor.

---
+++ core/algorithm/fedpor.py
@@ -0,0 +1,120 @@
+import copy
+import logging
+import traceback
+
+import ray
+import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+import wandb
+
+from core.server.fedpor_server import FedPORServer
+
+FEDPOR_ARGS = {
+    'num_negatives': 4,
+    'item_hidden_dim': 32,
+    'negatives_candidates': 99,
+
+    'top_k': 10,
+    'tol': 0.0001,
+
+    'mu': 0.01,
+    'lambda': 0.01,
+    'decay_rate': 0.97,
+    'regular': 'l1',
+    'vary_param': 'tanh',
+
+    'lr_args': 1e3,
+    'lr_network': 0.5,
+    'l2_regularization': 1e-4,
+}
+
+
+def run(args):
+    server = FedPORServer(args, FEDPOR_ARGS)
+    server.allocate_init_status()
+
+    logging.info(f"Creates {args['method']} server successfully.")
+
+    for communication_round in range(1, args['num_rounds']+1):
+        logging.info(f"{'='*20} Round {communication_round} starts {'='*20}")
+        participants = server.select_participants()
+
+        train_data_future = server.dataset.sample_train_data.remote()
+        server.train_on_round(participants)
+
+        round_loss = sum(sum(server.user_context[user_id]['loss']) / len(server.user_context[user_id]['loss']) for user_id in participants) / len(participants)
+
+        origin_params = copy.deepcopy(server.global_model.state_dict())
+        updated_params = server.aggregate(participants, ['item_commonality.weight'])
+
+        for _, user_context in server.user_context.items():
+            user_context['state_dict']['item_commonality.weight'] = copy.deepcopy(
+                                updated_params['item_commonality.weight'])
+
+        server_params = copy.deepcopy(server.global_model.state_dict())
+        server_params['item_commonality.weight'] = updated_params['item_commonality.weight'].data
+        server.global_model.load_state_dict(server_params)
+
+        val_hr, val_ndcg = server.test_on_round(server.val_data)
+        test_hr, test_ndcg = server.test_on_round(server.test_data)
+        com_hr, com_ndcg = server.test_commonality(server.test_data)
+        decouple_hr, decouple_ndcg, avg_item_personal = server.test_decouple(participants, server.test_data)
+
+        common_figure = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(updated_params['item_commonality.weight'][:100, :]))
+
+        average_figure = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(avg_item_personal['item_personality.weight'][:100, :]))
+
+        first_client_personal_figure = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(server.user_context[0]['state_dict']['item_personality.weight'][:100, :]))
+
+        first_client_common_figure = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(server.user_context[0]['state_dict']['item_commonality.weight'][:100, :]))
+
+        logging.info(
+            f"Val HR = {val_hr:.4f}, Val NDCG = {val_ndcg:.4f}, Test HR = {test_hr:.4f}, Test NDCG = {test_ndcg:.4f}"
+            f"Commonality HR = {com_hr:.4f}, Commonality NDCG = {com_ndcg:.4f},"
+            f"Decouple HR = {decouple_hr:.4f}, Decouple NDCG = {decouple_ndcg:.4f}"
+        )
+
+        wandb.log({
+            'commonality.weight': wandb.Image(common_figure),
+            'avg_personal.weight': wandb.Image(average_figure),
+            'first_personal.weight': wandb.Image(first_client_personal_figure),
+            'first_commonality.weight': wandb.Image(first_client_common_figure),
+
+            'round_loss': round_loss,
+            'val_hr': val_hr, 'val_ndcg': val_ndcg,
+            'test_hr': test_hr, 'test_ndcg': test_ndcg,
+            'commonality_hr': com_hr, 'commonality_ndcg': com_ndcg,
+            'decouple_hr': decouple_hr, 'decouple_ndcg': decouple_ndcg},
+            step=communication_round)
+
+        server.args['lr_network'] = server.args['lr_network'] * server.args['decay_rate']
+        server.args['lr_args'] = server.args['lr_args'] * server.args['decay_rate']
+
+        save_path = server.args['log_dir'] / f"{communication_round}" / f"{communication_round}.pth"
+
+
+        if args['save']:
+            if not save_path.parent.exists():
+                save_path.parent.mkdir(parents=True, exist_ok=True)
+
+                torch.save(
+                    {
+                        "origin_params": origin_params,
+                        "aggregate_params": updated_params,
+                        "updated_params": copy.deepcopy(server.global_model.state_dict()),
+                        "averaged_item_personal": avg_item_personal,
+
+                        "participants": participants,
+                        "user_context": server.user_context,
+                        "args": server.args,
+                        "data": [server.train_data, server.val_data, server.test_data],
+                    }, save_path
+                )
+
+        server.train_data = ray.get(train_data_future)
+        plt.close('all')
---
+++ core/client/fedpor_client.py
@@ -0,0 +1,118 @@
+import os
+import copy
+import logging
+
+import ray
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from core.client.base_client import BaseClient
+from utils.utils import initLogging, seed_anything
+
+class UserItemRatingDataset(Dataset):
+    def __init__(self, user_tensor, item_tensor, rating_tensor):
+        self.user_tensor = user_tensor
+        self.item_tensor = item_tensor
+        self.rating_tensor = rating_tensor
+
+    def __getitem__(self, index):
+        return self.user_tensor[index], self.item_tensor[index], self.rating_tensor[index]
+
+    def __len__(self):
+        return self.user_tensor.size(0)
+
+
+class FedRapLoss(torch.nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.crit = torch.nn.BCELoss()
+        self.independency = torch.nn.MSELoss()
+
+        if self.args['regular'] == 'l1':
+            self.reg = torch.nn.L1Loss()
+        else:
+            self.reg = torch.nn.MSELoss()
+
+    def forward(self, ratings_pred, ratings, item_personality, item_commonality):
+        if self.args['regular'] == 'none':
+            self.args['mu'] = 0
+
+        if self.args['regular'] == 'nuc':
+            third = torch.norm(item_commonality, p='nuc')
+
+        elif self.args['regular'] == 'inf':
+            third = torch.norm(item_commonality, p=float('inf'))
+
+        else:
+            dummy_target = torch.zeros_like(item_commonality, requires_grad=False)
+            third = self.reg(item_commonality, dummy_target)
+
+        loss = self.crit(ratings_pred, ratings) \
+               - self.args['lambda'] * self.independency(item_personality, item_commonality) \
+               + self.args['mu'] * third
+
+        # loss = self.crit(ratings_pred, ratings)
+        return loss
+
+
+@ray.remote
+class FedPORActor(BaseClient):
+    def __init__(self, args) -> None:
+        super().__init__(args)
+        self.device = torch.device(self.args['device'])
+        seed_anything(args['seed'])
+        initLogging(args['log_dir'] / f"client_{os.getpid()}.log", stream=False)
+
+    def train(self, model, user_data):
+        user_context, train_data = user_data[0], user_data[1]
+
+        user_model = copy.deepcopy(model)
+        user_model.load_state_dict(user_context['state_dict'])
+
+        user_model = user_model.to(self.device)
+        optimizer = torch.optim.SGD([
+            {'params': user_model.user_embedding.parameters(), 'lr': self.args['lr_network']},
+            {'params': user_model.item_personality.parameters(), 'lr': self.args['lr_args']},
+            {'params': user_model.item_commonality.parameters(), 'lr': self.args['lr_args']},
+        ], weight_decay=self.args['l2_regularization'])
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+
+        dataloader = DataLoader(
+            dataset=UserItemRatingDataset(
+                    user_tensor=torch.LongTensor(train_data[0]),
+                    item_tensor=torch.LongTensor(train_data[1]),
+                    rating_tensor=torch.LongTensor(train_data[2])),
+            batch_size=self.args['batch_size'],
+            shuffle=True
+        )
+
+        user_model.train()
+        training_loss = []
+        for epoch in range(self.args['local_epoch']):
+            epoch_loss, samples = 0, 0
+            for users, items, ratings in dataloader:
+                loss_fn = FedRapLoss(self.args)
+
+                users, items, ratings = users.to(self.device), items.to(self.device), ratings.float().to(self.device)
+
+                optimizer.zero_grad()
+                ratings_pred, items_personality, items_commonality = user_model(items)
+
+                loss = loss_fn(ratings_pred.view(-1), ratings, items_personality, items_commonality)
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+
+                epoch_loss += loss.item() * len(users)
+                samples += len(users)
+            training_loss.append(epoch_loss / samples)
+
+            # check convergence
+            if epoch > 0 and abs(training_loss[epoch] - training_loss[epoch - 1]) / abs(
+                    training_loss[epoch - 1]) < self.args['tol']:
+                break
+
+        user_model.to('cpu')
+        return user_context['user_id'], user_model, training_loss
