81d59e8708d72a9dd706db8f2b9b7fef6f8909ef
lokinko <lokinko.cs@gmail.com>
2025-02-17 08:43:40 +0000

    style: modify the style to be easy followed.

--- core/algorithm/fedporplus.py
+++ core/algorithm/fedporplus.py
@@ -39,58 +39,57 @@ def run(args):
         logging.info(f"{'='*20} Round {communication_round} starts {'='*20}")
         participants = server.select_participants()
 
+        # get async train data before training
         train_data_future = server.dataset.sample_federated_train_data.remote()
         server.train_on_round(participants)
 
-        round_loss = sum(sum(server.user_context[user_id]['loss']) / len(server.user_context[user_id]['loss']) for user_id in participants) / len(participants)
+        # calculate round loss
+        round_loss = 0.0
+        for user_id in participants:
+            round_loss += sum(server.user_context[user_id]['loss']) / len(server.user_context[user_id]['loss'])
+        wandb.log({'round_loss': round_loss / len(participants)}, step=communication_round)
 
-        origin_params = copy.deepcopy(server.global_model.state_dict())
-        updated_params = server.aggregate(participants, ['item_commonality.weight'])
+        # aggregate global parameters
+        global_params = server.aggregate(participants, ['item_commonality.weight'])
 
+        # update user context
         for _, user_context in server.user_context.items():
             user_context['state_dict']['item_commonality.weight'] = copy.deepcopy(
-                                updated_params['item_commonality.weight'])
+                                global_params['item_commonality.weight'])
 
+        # update global model
         server_params = copy.deepcopy(server.global_model.state_dict())
-        server_params['item_commonality.weight'] = updated_params['item_commonality.weight'].data
+        server_params['item_commonality.weight'] = global_params['item_commonality.weight'].data
         server.global_model.load_state_dict(server_params)
 
+        # test on validation and test data
         val_hr, val_ndcg = server.test_on_round(server.val_data)
         test_hr, test_ndcg = server.test_on_round(server.test_data)
+        logging.info(f"Val HR = {val_hr:.4f}, Val NDCG = {val_ndcg:.4f}, Test HR = {test_hr:.4f}, Test NDCG = {test_ndcg:.4f}")
+        wandb.log({'val_hr': val_hr, 'val_ndcg': val_ndcg, 'test_hr': test_hr, 'test_ndcg': test_ndcg}, step=communication_round)
+
+        # test commonality
         com_hr, com_ndcg = server.test_commonality(server.test_data)
-        decouple_hr, decouple_ndcg, avg_item_personal = server.test_decouple(participants, server.test_data)
+        logging.info(f"Commonality HR = {com_hr:.4f}, Commonality NDCG = {com_ndcg:.4f}")
+        wandb.log({'com_hr': com_hr, 'com_ndcg': com_ndcg}, step=communication_round)
 
         common_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(updated_params['item_commonality.weight'][:100, :]))
+        sns.heatmap(torch.abs(global_params['item_commonality.weight'][:, :]))
 
-        average_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(avg_item_personal['item_personality.weight'][:100, :]))
+        client_id = 100
+        client_personal_fig = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(server.user_context[client_id]['state_dict']['item_personality.weight'][:, :]))
 
-        first_client_personal_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(server.user_context[0]['state_dict']['item_personality.weight'][:100, :]))
-
-        first_client_common_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(server.user_context[0]['state_dict']['item_commonality.weight'][:100, :]))
-
-        logging.info(
-            f"Val HR = {val_hr:.4f}, Val NDCG = {val_ndcg:.4f}, Test HR = {test_hr:.4f}, Test NDCG = {test_ndcg:.4f}, "
-            f"Commonality HR = {com_hr:.4f}, Commonality NDCG = {com_ndcg:.4f}, "
-            f"Decouple HR = {decouple_hr:.4f}, Decouple NDCG = {decouple_ndcg:.4f}"
-        )
+        client_common_fig = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(server.user_context[client_id]['state_dict']['item_commonality.weight'][:, :]))
 
         wandb.log({
             'commonality.weight': wandb.Image(common_figure),
-            'avg_personal.weight': wandb.Image(average_figure),
-            'first_personal.weight': wandb.Image(first_client_personal_figure),
-            'first_commonality.weight': wandb.Image(first_client_common_figure),
-
-            'round_loss': round_loss,
-            'val_hr': val_hr, 'val_ndcg': val_ndcg,
-            'test_hr': test_hr, 'test_ndcg': test_ndcg,
-            'commonality_hr': com_hr, 'commonality_ndcg': com_ndcg,
-            'decouple_hr': decouple_hr, 'decouple_ndcg': decouple_ndcg},
+            'first_personal.weight': wandb.Image(client_personal_fig),
+            'first_commonality.weight': wandb.Image(client_common_fig)},
             step=communication_round)
 
+        # update learning rate decay
         server.args['lr_network'] = server.args['lr_network'] * server.args['decay_rate']
         server.args['lr_args'] = server.args['lr_args'] * server.args['decay_rate']
 
@@ -100,24 +99,21 @@ def run(args):
 
         save_path = server.args['log_dir'] / f"{communication_round}" / f"{communication_round}.pth"
 
-
-        if args['save']:
-            if not save_path.parent.exists():
-                save_path.parent.mkdir(parents=True, exist_ok=True)
-
-                torch.save(
-                    {
-                        "origin_params": origin_params,
-                        "aggregate_params": updated_params,
-                        "updated_params": copy.deepcopy(server.global_model.state_dict()),
-                        "averaged_item_personal": avg_item_personal,
-
-                        "participants": participants,
-                        "user_context": server.user_context,
-                        "args": server.args,
-                        "data": [server.train_data, server.val_data, server.test_data],
-                    }, save_path
-                )
-
         server.train_data = ray.get(train_data_future)
         plt.close('all')
+
+    # save key information in the round
+    if args['save']:
+        if not save_path.parent.exists():
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+
+            torch.save(
+                {
+                    "aggregate_params": global_params,
+                    "updated_params": copy.deepcopy(server.global_model.state_dict()),
+                    "participants": participants,
+                    "user_context": server.user_context,
+                    "args": server.args,
+                    "data": [server.train_data, server.val_data, server.test_data],
+                }, save_path
+            )
--- core/algorithm/fedrap.py
+++ core/algorithm/fedrap.py
@@ -1,6 +1,5 @@
 import copy
 import logging
-import traceback
 
 import ray
 import torch
@@ -33,66 +32,63 @@ def run(args):
     server = FedRAPServer(args, FEDRAP_ARGS)
     server.allocate_init_status()
 
-
     logging.info(f"Creates {args['method']} server successfully.")
-
-    item_commonality = torch.nn.Embedding(num_embeddings=server.args['num_items'], embedding_dim=server.args['item_hidden_dim'])
 
     for communication_round in range(1, args['num_rounds']+1):
         logging.info(f"{'='*20} Round {communication_round} starts {'='*20}")
         participants = server.select_participants()
 
+        # get async train data before training
         train_data_future = server.dataset.sample_federated_train_data.remote()
-        server.train_on_round(item_commonality, participants)
-        round_loss = sum(sum(server.user_context[user_id]['loss']) / len(server.user_context[user_id]['loss']) for user_id in participants) / len(participants)
+        server.train_on_round(participants)
 
-        origin_params = copy.deepcopy(server.global_model.state_dict())
-        updated_params = server.aggregate(participants, ['item_commonality.weight'])
+        # calculate round loss
+        round_loss = 0.0
+        for user_id in participants:
+            round_loss += sum(server.user_context[user_id]['loss']) / len(server.user_context[user_id]['loss'])
+        wandb.log({'round_loss': round_loss / len(participants)}, step=communication_round)
 
+        # aggregate global parameters
+        global_params = server.aggregate(participants, ['item_commonality.weight'])
+
+        # update user context
         for _, user_context in server.user_context.items():
             user_context['state_dict']['item_commonality.weight'] = copy.deepcopy(
-                                updated_params['item_commonality.weight'])
+                                global_params['item_commonality.weight'])
 
+        # update global model
         server_params = copy.deepcopy(server.global_model.state_dict())
-        server_params['item_commonality.weight'] = updated_params['item_commonality.weight'].data
+        server_params['item_commonality.weight'] = global_params['item_commonality.weight'].data
         server.global_model.load_state_dict(server_params)
 
+        # test on validation and test data
         val_hr, val_ndcg = server.test_on_round(server.val_data)
         test_hr, test_ndcg = server.test_on_round(server.test_data)
+        logging.info(f"Val HR = {val_hr:.4f}, Val NDCG = {val_ndcg:.4f}, Test HR = {test_hr:.4f}, Test NDCG = {test_ndcg:.4f}")
+        wandb.log({'val_hr': val_hr, 'val_ndcg': val_ndcg, 'test_hr': test_hr, 'test_ndcg': test_ndcg}, step=communication_round)
+
+        # test commonality
         com_hr, com_ndcg = server.test_commonality(server.test_data)
-        decouple_hr, decouple_ndcg, avg_item_personal = server.test_decouple(participants, server.test_data)
+        logging.info(f"Commonality HR = {com_hr:.4f}, Commonality NDCG = {com_ndcg:.4f}")
+        wandb.log({'com_hr': com_hr, 'com_ndcg': com_ndcg}, step=communication_round)
 
         common_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(updated_params['item_commonality.weight'][:, :]))
+        sns.heatmap(torch.abs(global_params['item_commonality.weight'][:, :]))
 
-        average_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(avg_item_personal['item_personality.weight'][:, :]))
+        client_id = 100
+        client_personal_fig = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(server.user_context[client_id]['state_dict']['item_personality.weight'][:, :]))
 
-        first_client_personal_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(server.user_context[0]['state_dict']['item_personality.weight'][:, :]))
-
-        first_client_common_figure = plt.figure(figsize=(10, 10))
-        sns.heatmap(torch.abs(server.user_context[0]['state_dict']['item_commonality.weight'][:, :]))
-
-        logging.info(
-            f"Val HR = {val_hr:.4f}, Val NDCG = {val_ndcg:.4f}, Test HR = {test_hr:.4f}, Test NDCG = {test_ndcg:.4f}, "
-            f"Commonality HR = {com_hr:.4f}, Commonality NDCG = {com_ndcg:.4f}, "
-            f"Decouple HR = {decouple_hr:.4f}, Decouple NDCG = {decouple_ndcg:.4f}"
-        )
+        client_common_fig = plt.figure(figsize=(10, 10))
+        sns.heatmap(torch.abs(server.user_context[client_id]['state_dict']['item_commonality.weight'][:, :]))
 
         wandb.log({
             'commonality.weight': wandb.Image(common_figure),
-            'avg_personal.weight': wandb.Image(average_figure),
-            'first_personal.weight': wandb.Image(first_client_personal_figure),
-            'first_commonality.weight': wandb.Image(first_client_common_figure),
-
-            'round_loss': round_loss,
-            'val_hr': val_hr, 'val_ndcg': val_ndcg,
-            'test_hr': test_hr, 'test_ndcg': test_ndcg,
-            'commonality_hr': com_hr, 'commonality_ndcg': com_ndcg,
-            'decouple_hr': decouple_hr, 'decouple_ndcg': decouple_ndcg},
+            'first_personal.weight': wandb.Image(client_personal_fig),
+            'first_commonality.weight': wandb.Image(client_common_fig)},
             step=communication_round)
 
+        # update learning rate decay
         server.args['lr_network'] = server.args['lr_network'] * server.args['decay_rate']
         server.args['lr_args'] = server.args['lr_args'] * server.args['decay_rate']
 
@@ -102,40 +98,21 @@ def run(args):
 
         save_path = server.args['log_dir'] / f"{communication_round}" / f"{communication_round}.pth"
 
+        server.train_data = ray.get(train_data_future)
+        plt.close('all')
 
-        if args['save']:
-            if not save_path.parent.exists():
-                save_path.parent.mkdir(parents=True, exist_ok=True)
+    # save key information in the round
+    if args['save']:
+        if not save_path.parent.exists():
+            save_path.parent.mkdir(parents=True, exist_ok=True)
 
             torch.save(
                 {
-                    "origin_params": origin_params,
-                    "aggregate_params": updated_params,
+                    "aggregate_params": global_params,
                     "updated_params": copy.deepcopy(server.global_model.state_dict()),
-                    "averaged_item_personal": avg_item_personal,
-
                     "participants": participants,
                     "user_context": server.user_context,
                     "args": server.args,
                     "data": [server.train_data, server.val_data, server.test_data],
                 }, save_path
             )
-            if not save_path.parent.exists():
-                save_path.parent.mkdir(parents=True, exist_ok=True)
-
-                torch.save(
-                    {
-                        "origin_params": origin_params,
-                        "aggregate_params": updated_params,
-                        "updated_params": copy.deepcopy(server.global_model.state_dict()),
-                        "averaged_item_personal": avg_item_personal,
-
-                        "participants": participants,
-                        "user_context": server.user_context,
-                        "args": server.args,
-                        "data": [server.train_data, server.val_data, server.test_data],
-                    }, save_path
-                )
-
-        server.train_data = ray.get(train_data_future)
-        plt.close('all')
